3c68e57fcebbba24aa88dd98c8b02c5c442d07bb
NeerajChouhan337 <160119497+NeerajChouhan337@users.noreply.github.com>
2024-09-23 22:13:02 +0530

    SignIn/SignUp && Login/Header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Body from "./components/Body";
 
 function App() {
-  return <div className="text-3xl text-slate-800 font-semibold ">NetFlix Gpt</div>;
+  return (
+    <div>
+      <Body/>
+    </div>
+  )
 }
 
 export default App;
